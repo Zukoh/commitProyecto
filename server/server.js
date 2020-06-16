b6515550ab7fe41c9e3a5e32489a82f26d1bbbd0
Alexis Crespo <alexis.a.crespo@gmail.com>
2020-06-16 09:31:48 -0300

    update masivo

--- server/server.js
+++ server/server.js
@@ -1,85 +1,231 @@
 const express = require("express");
+const path = require("path");
+const expHbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+
+const valid = require("./valid");
+
 const app = express();
-const path= require("path");
-const bodyParser = require ("body-parser");
 
-// Middlewares 
-app.use(express.static(path.join(__dirname, "../client")));
-app.use(bodyParser.json());
+app.set("view engine", "handlebars");
 
-let userList = [];
+app.engine("handlebars", expHbs({
+  defaultLayout: "main",
+  layoutsDir: path.join(__dirname, "views/layouts")
+}));
 
-app.get("/", (req,res)=>{
-   res.sendFile(path.join(__dirname, "../client/index.html"));
+app.set("views", path.join(__dirname, "views"));
+
+// Ruta base de recursos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// Body Parser para Content-Type "application/x-www-form-urlencoded"
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Landing page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"))
+});
+
+// Página de registro
+app.get("/register", (req, res) => {
+  res.render("register", { layout: "logeos" });
+});
+
+// Endpoint que registra user nuevx
+app.post("/register", (req, res) => {
+  // 1. Validar datos de registro
+  valid.getUser(req.body.user, result => {
+
+    // Si no se pudo consultar a la DB renderizo signup con mensaje de error
+    if (!result.success) {
+      res.render("register", {
+        layout: "logeos",
+        message: {
+          class: "failure",
+          text: "Sorry, can't register now, retry later."
+        }
+      });
+      return;
+    }
+
+    // Si el usuario ya existe renderizo signup con mensaje de error
+    if (result.user) {
+      res.render("register", {
+        layout: "logeos",
+        message: {
+          class: "failure",
+          text: "Disculpe, el usuario ya esta en uso.."
+        }
+      });
+      return;
+    }
+
+    // Si el password está mal ingresado renderizo signup con mensaje de error
+    if (!req.body.pass || req.body.pass !== req.body.passRepeat) {
+      res.render("register", {
+        layout: "logeos",
+        message: {
+          class: "failure",
+          text: "Las contraseñas que escribio no coinciden.."
+        }
+      });
+      return;
+    }
+
+    // Procesamos alta de usuarix
+    valid.registerUser(req.body.user, req.body.pass, req.body.dni, req.body.edad, req.body.reserva, req.body.local, result => {
+
+      if (result) {
+
+        // Si se pudo registrar renderizo login con mensaje de éxito
+        res.render("login", {
+          layout: "logeos", message: {
+            class: "success",
+            text: "Usuario registrado con exito."
+          }
+        });
+
+      } else {
+
+        // Si no se pudo registrar renderizo signup con mensaje de error
+        res.render("register", {
+          layout: "logeos",
+          message: {
+            class: "failure",
+            text: "Disculpe no pudimos registrar al usuario, intente mas tarde."
+          }
+        });
+
+      }
+
+    });
+
+
+  });
+
 });
 
 app.get("/login", (req, res) =>{
-    res.sendFile(path.join(__dirname, "../client/login.html"));
+  res.render("login", { layout: "logeos" });
 })
 
+app.get("/eventos", (req, res) =>{
+  res.render("eventos", { layout: "main" })
+})
+
+app.get("/home", (req, res) =>{
+  res.render("home", { layout: "main" })
+})
+
+app.get("/homelocal", (req, res) =>{
+  res.render("homelocal", { layout: "main" })
+})
+
+app.get("/boliches", (req, res) =>{
+  res.render("boliches", { layout: "main" })
+})
+
+app.get("/canchas", (req, res) =>{
+  res.render("canchas", { layout: "main" })
+})
+
+app.get("/local", (req, res) =>{
+  res.render("local", { layout: "main" })
+})
+
+app.get("/homelocal", (req, res)=>{
+  valid.dameReservas("Los Maizales", result =>{
+    
+    if (result.valid){
+      
+      res.render("homelocal", {
+        layout: "main", 
+        message: {
+            class:"bloque-reserva",
+            nombre: "hola?",
+            hora: result.hora,
+            montopagado:result.montopagado,
+            cantidad:result.cantpersonas
+      }
+    })
+    }
+  } )    
+})
+app.get("/reservar", (req, res)=>{
+  res.render("reservar", {layout: "main"})
+  
+})
+
+
+app.get("/reservar2", (req, res)=>{
+  res.render("reservar2", {layout: "main"})
+  
+})
+app.get("/reservar3", (req, res)=>{
+  res.render("reservar3", {layout: "main"})
+  
+})
+app.get("/reservar4", (req, res)=>{
+  res.render("reservar4", {layout: "main"})
+  
+})
+
+
+
+// Endpoint que valida user/pass (form)
 app.post("/login", (req, res) => {
-    if(!req.body){
-        res.status(400).send("No se encontraron datos..");
-        return;
+
+  valid.login(req.body.user, req.body.pass, result => {
+
+    if (result.valid) {
+      // Renderizado de home con datos de personas
+      if(result.msg === "reserva"){
+      res.render("home", { layout: "main", message: result.msg })
+      }
+      else{
+        res.render("homelocal", { layout: "main", message: result.msg })
+      }
+    } else {
+      // Se retorna el login con el error
+      res.render("login", { layout: "logeos", message: result.msg });
     }
-    if(!req.body.username || !req.body.password ){
-        res.status(400).send("No se recibieron datos..");
-        return;
-    }
-    
-    const foundUsers= userList.filter(user => user.username === req.body.username);
-    if (foundUsers.length > 0){
-        const user = foundUsers[0];
-        if(user.password === req.body.password ){
-            res.status(200).send("logeado");
-        } else{
-            res.status(400).send("La contraseña no coincide");
-        }
-    }else{
-             res.status(400).send("no existe el usuariio");
-        }
-    
-    
+
+  });
+
 });
-app.get("/register", (req, res) =>{
-    res.sendFile(path.join(__dirname, "../client/register.html"));
+
+app.post("/local", (req, res)=>{
+  valid.postLocal(req.body.nombre, req.body.direccion, req.body.minReserva, req.body.diaDesde, req.body.horarios, result =>{
+    if (result.valid){
+      res.render("local", {
+        layout: "main", 
+        message: {
+            class:"localpost",
+            text: "Su local se guardo correctamente :)"
+      }
+    })
+    }
+  } )    
 })
 
-app.post("/register", (req, res) =>{
-    if(!req.body){
-        res.status(400).send("No se encontraron datos");
-        return;
+app.post("/reservar", (req, res)=>{
+  valid.newReserva(req.body.nombre, req.body.montopag, req.body.cantpersonas, req.body.hora, result =>{
+    if (result.valid){
+      res.render("reservar", {
+        layout: "main", 
+        message: {
+            class:"localpost",
+            text: "Su reserva se guardo correctamente :)"
+      }
+    })
     }
-    if(!req.body.username || !req.body.password){
-        res.status(400).send("No se econtraron los datos recibidos");
-        return;
-    }
+  } )    
+})
 
 
-    // Si recibi los datos:
 
-    userList.push({
-        username: req.body.username,
-        password: req.body.password,
-        mail : req.body.mail,
-        dni : req.body.dni,
-        edad : req.body.edad,
-        tipouser : req.body.tipouser,
-        tipouser2 : req.body.tipouser2
-    });
-    
-    res.status(200).send("usuarix registradx");
+// Inicio del servidor
+app.listen(5555, () => {
+  console.log(`Servidor iniciado en http://localhost:5555/ ...`)
 });
-
-
-
-
-
-
-
-
-
-app.listen("4444", () =>{
-    console.log("Servidor corriendo en http://localhost:4444")
-});
-
---
+++ server/valid.js
@@ -0,0 +1,301 @@
+const mongodb = require("mongodb");
+const mongoURL = "mongodb+srv://zukito:argentina2254@cluster0-tu8na.mongodb.net/proyecto?retryWrites=true&w=majority";
+
+/**
+ * Función que valida user/pass. Retorna un objeto con un boolean que indica si las credenciales
+ * son válidas y un string con un mensaje para errores.
+ * 
+ * @param {string} user Username
+ * @param {string} pass Password
+ * @param {function} cbResult Callback: function(result: { valid: boolean, msg: string })
+ */
+const login = (user, pass, cbResult) => {
+  // Nos conectamos al servidor de MongoDB
+  mongodb.MongoClient.connect(mongoURL, (err, client) => {
+
+    if (err) {
+
+      // Si no me pude conectar al server, retorno el false con un mensaje apropiado para el front
+      cbResult({
+        valid: false,
+        msg: "Sorry, site is under maintenance now, retry later."
+      });
+
+    } else {
+
+      const proyectoDB = client.db("proyecto");
+      const usersCollection = proyectoDB.collection("users");
+
+      usersCollection.findOne({ user: user, password: pass }, (err, foundUser) => {
+
+        if (err) {
+
+          // Si no pude consultar la colección, también retorno false con un mensaje, pero
+          // lo hago ligeramente diferente al anterior para poder distinguir errores.
+          cbResult({
+            valid: false,
+            msg: "Sorry, the site is under maintenance now, retry later."
+          });
+
+        } else {
+
+          // Si pude consultar los datos, valido si encontré esa combinación usr/pwd o no.
+          if (!foundUser) {
+            cbResult({
+              valid: false,
+              msg: "Invalid user/password."
+            });
+          } else {
+            // Si valida ok, me fijo que tipo de usuario es para saber que renderizar
+            if(foundUser.userReserva === "on"){
+              cbResult({
+                valid: true,
+                msg: "reserva"
+              });
+            }else{
+            cbResult({
+              valid: true,
+              msg:"local"
+            });
+          }
+          }
+
+        }
+
+        client.close();
+      });
+
+    }
+
+  });
+}
+
+/**
+ * Función que consulta usuarix en la DB y retorna los datos
+ * 
+ * @param {string} username Nombre de usuarix
+ * @param {function} cbResult Callback: function(result: {
+ *  success: boolean,
+ *  user: {
+ *    user: string,
+ *    password: string
+ *  }
+ * })
+ */
+const getUser = (username, cbResult) => {
+
+  mongodb.MongoClient.connect(mongoURL, (err, client) => {
+
+    if (err) {
+
+      cbResult({
+        success: false
+      });
+
+    } else {
+
+      const proyectoDB = client.db("proyecto");
+      const usersCollection = proyectoDB.collection("users");
+
+      usersCollection.findOne({ user: username }, (err, result) => {
+
+        if (err) {
+          cbResult({
+            success: false
+          });
+        } else {
+          cbResult({
+            success: true,
+            user: result
+          });
+        }
+
+        client.close();
+
+      });
+
+    }
+
+  });
+
+}
+
+/**
+ * Función que registra nuevx usuarix (asume username y password validados)
+ * 
+ * @param {string} username Username
+ * @param {string} password Password
+ * @param {function} cbResult Callback: function(result: boolean)
+ */
+const registerUser = (username, password, dni, edad, userReserva, userLocal, cbResult) => {
+  mongodb.MongoClient.connect(mongoURL, (err, client) => {
+
+    if (err) {
+
+      // Si hay error de conexión, retornamos el false
+      // (no cerramos conexión porque no se logró abrir)
+      cbResult(false);
+
+    } else {
+
+      const proyectoDB = client.db("proyecto");
+      const usersCollection = proyectoDB.collection("users");
+
+      const newUser = {
+        user: username,
+        password: password,
+        dni: dni,
+        edad: edad,
+        userReserva: userReserva,
+        userLocal: userLocal,
+      };
+
+      // Insertamos el user en la DB
+      usersCollection.insertOne(newUser, (err, result) => {
+        
+        if (err) {
+          cbResult(false);
+        } else {
+          cbResult(true);
+          // console.log(result);
+        }
+
+        client.close();
+      });
+
+    }
+
+  });
+}
+
+
+const postLocal = (nombre, dir, preserva, dispDias, dispHoras, cbR) =>{
+  //Me conecto a mongoDB
+  mongodb.MongoClient.connect(mongoURL, (err, client) =>{
+    if(err){
+      cbR({
+        valid:false,
+        msg:"No pudimos acceder en este momento intente mas tarde.."
+      })
+    } else {
+      
+      const proyectoDB = client.db("proyecto");
+      const localCol = proyectoDB.collection("locales");
+
+      const newLocal = {
+        nombre: nombre,
+        direccion: dir,
+        precioreserva: preserva,
+        dispDias: dispDias,
+        dispHoras : dispHoras
+      };
+
+      localCol.insertOne(newLocal, (err, resultado) =>{
+        if (err){
+          cbR({
+            valid:false,
+            msg:"No pudimos guardar el local en este momento"
+          });
+        } else{
+            cbR({
+              valid:true,
+              msg: "Se guardo correctamente los datos de tu local"
+            });
+        } 
+        client.close();
+      })
+    }
+  })
+}
+
+const newReserva = (nombre, montopag, cantpersonas, hora, cbR) =>{
+  //Me conecto a mongoDB
+  mongodb.MongoClient.connect(mongoURL, (err, client) =>{
+    if(err){
+      cbR({
+        valid:false,
+        msg:"No pudimos acceder en este momento intente mas tarde.."
+      })
+    } else {
+      
+      const proyectoDB = client.db("proyecto");
+      const reservaCol = proyectoDB.collection("reservas");
+
+      const newReserva = {
+        nombre: nombre,
+        montopagado: montopag,
+        cantpersonas: cantpersonas,
+        hora : hora,
+        local: "Los Maizales"
+      };
+
+      reservaCol.insertOne(newReserva, (err, resultado) =>{
+        if (err){
+          cbR({
+            valid:false,
+            msg:"No pudimos guardar el local en este momento"
+          });
+        } else{
+            cbR({
+              valid:true,
+              msg: "Se guardo correctamente los datos de tu reserva"
+            });
+        } 
+        client.close();
+      })
+    }
+  })
+}
+
+const dameReservas = (nombre, cbResult) => {
+
+  mongodb.MongoClient.connect(mongoURL, (err, client) => {
+
+    if (err) {
+
+      cbResult({
+        success: false
+      });
+
+    } else {
+
+      const proyectoDB = client.db("proyecto");
+      const reservaCol = proyectoDB.collection("reservas");
+
+      reservaCol.findOne({ nombre: nombre }, (err, result) => {
+
+        if (err) {
+          cbResult({
+            valid: false
+          });
+        } else {
+          console.log(result)
+          cbResult({
+            valid: true,
+            nombre: result.nombre,
+            montopagado: result.montopagado,
+            cantpersonas: result.cantpersonas,
+            hora:result.hora
+          });
+        }
+
+        client.close();
+
+      });
+
+    }
+
+  });
+
+}
+
+
+
+module.exports = {
+  login,
+  getUser,
+  registerUser,
+  postLocal,
+  newReserva,
+  dameReservas
+}
